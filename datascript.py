4332d5e36cb27a742507d906f37bfe6373593ceb
rajibrhasan <1921834@gmail.com>
2021-04-26 23:42:25 +0600

    Updated database

--- datascript.py
+++ datascript.py
@@ -18,31 +18,37 @@ details = ["Knowledge", "Requirement Analysis", "Requirement Analysis", "Design"
            "Experiment and Analysis", "Community Engagement and Engineering", "Teamwork", "Communication",
            "Self-Motivated", "Ethics", "Process Management"]
 
-for p in programList:
-    for i in range(1, 14):
-        plonum="PLO"+str(i)
+# for p in programList:
+#  for i in range(1, 14):
+#     plonum="PLO"+str(i)
 
-        plo = PLO_T(PLONum=plonum, ProgramID=p, Details=details[i - 1])
-        plo.save()
-
+#    plo = PLO_T(ploNum=plonum, program=p, details=details[i - 1])
+#   plo.save()
 
 
 # Add Faculty Information
 
 
-fnames = ["Mahady", "Sadita", "Md. Abu", "Romasa", "Ferdows","Bijoy R.", "Mohammad Motiur", "Raihan Bin", "Faisal M.",
-          "Asif Bin", "Subrata Kumar", "Mohammad Noor", "Farruk", "Sabrina", "Sanzar Adnan"]
+#fnames = ["Mahady", "Sadita", "Md. Abu", "Romasa", "Ferdows", "Bijoy R.", "Mohammad Motiur", "Raihan Bin", "Faisal M.",
+          #"Asif Bin", "Subrata Kumar", "Mohammad Noor", "Farruk", "Sabrina", "Sanzar Adnan"]
 
-lnames = ["Hasan", "Ahmed", "Sayed", "Qasim", "Zahid", "Arif", "Rahman", "Rafique", "Uddin", "Khaled", "Dey", "Nabi",
-         "Ahmed", "Alam", "Alam"]
+#lnames = ["Hasan", "Ahmed", "Sayed", "Qasim", "Zahid", "Arif", "Rahman", "Rafique", "Uddin", "Khaled", "Dey", "Nabi",
+          #"Ahmed", "Alam", "Alam"]
 
-id = 4250
+#id = 4250
 
-i = 0
+#i = 0
+
+id =4201
+
+i=0
+
+fnames = [ "Naheem", "Md. ", "Susmita", "Shahriar", "Kamrul","Nabila", "Abul", "Nadim", "Farzana"]
+lnames = ["Mahtab","Saifuddin","Mandal","Kabir", "Islam","Maruf","Bashar","Jahangir","Chowdhury"]
 
 for name in fnames:
-    d=Department_T.objects.get(pk="CSE")
-    f = Faculty_T(FacultyID=id, FirstName=name, LastName=lnames[i], EmployeeType="F", DepartmentID=d)
+    d = Department_T.objects.get(pk="BBA")
+    f = Faculty_T(facultyID=id, firstName=name, lastName=lnames[i], employeeType="F", department=d)
     f.save()
-    id=id+1
-    i=i+1
+    id = id + 1
+    i = i + 1
